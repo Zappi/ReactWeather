f78bee96c9215515cbdb66b4e1ae9419bb75aa47
Jerry <jerry.tamlander@helsinki.fi>
2017-08-13 22:31:06 +0300

    Added some functionality to search bar

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -5,16 +5,35 @@ export default class SearchBar extends React.Component {
     super(props);
 
     this.state = {
-      searhText: '',
-    }
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert("Jännää " + this.state.value);
+    event.preventDefault();
   }
 
   render(){
     return(
+      <form onSubmit={this.handleSubmit}>
         <div className="row center-block">
           <div className="col-md-10 col-md-offset-1">
               <div className="input-group col-md-12">
-                <input type="text" className="form-control input-lg" placeholder="Give a city name"/>
+                <input
+                type="text"
+                className="form-control input-lg"
+                placeholder="Give a city name"
+                value={this.state.value}
+                onChange={this.handleChange}
+                />
                 <span className="input-group-btn">
                   <button className="btn search-button btn-lg" type="button">
                     <i className="glyphicon glyphicon-search"></i>
@@ -23,7 +42,7 @@ export default class SearchBar extends React.Component {
             </div>
           </div>
         </div>
-
+      </form>
     );
   }
 }
